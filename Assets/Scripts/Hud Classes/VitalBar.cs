3a3c87419a70b7ea878e7826656523b4027799e2
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2013-01-13 18:00:51 -0500

    vid 44 finished

---
+++ Assets/Scripts/Hud Classes/VitalBar.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class VitalBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnEnable(){
+		
+	}
+	
+	public void OnDisable(){
+		
+	}
+}
